8315459e62aea642751d299406a77f1c80916705
elliott-king <8884571+elliott-king@users.noreply.github.com>
2020-03-18 16:22:57 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,32 @@
+require "pry"
+
 class Dog < ActiveRecord::Base
 end
+
+# has: 
+# inheritence
+#   inherits from ActiveRecord::Base
+# attributes
+#   has a name and a breed
+#   has an id that defaults to `nil` on initialization
+#   accepts key value pairs as arguments to initialize
+# .create
+#   takes in a hash of attributes and uses metaprogramming to create a new dog object. Then it uses the #save method to save that dog to the database
+# .save
+#   saves an instance of the dog class to the database and then sets the given dogs `id` attribute
+# .update
+#   updates the record associated with a given instance
+# .find_or_create_by
+#   creates a dog if it does not already exist
+# .find_by_name
+#   returns a dog that matches the name from the DB
+# .find_by_id
+#   returns a dog that matches the id from the DB
+
+# has not:
+# .create_table
+#   creates the dogs table in the database
+# .drop_table
+#   drops the dogs table from the database
+# .new_from_db
+#   creates an instance with corresponding attribute values
